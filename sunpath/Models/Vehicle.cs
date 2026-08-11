94484968e79502570ab291c7c36354198f77b9dd
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-11 12:35:35 +0330

    add modal

---
+++ sunpath/Models/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sunpath.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string PlateNumber { get; set; }
+        public string Status { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Speed { get; set; }
+        public double Heading { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
