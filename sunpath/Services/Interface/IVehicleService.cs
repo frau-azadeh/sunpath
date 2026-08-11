e158cdbb99433cf09fbdd8d3ff3ca737cf9e33df
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-11 13:11:07 +0330

    add interface

---
+++ sunpath/Services/Interface/IVehicleService.cs
@@ -0,0 +1,15 @@
+﻿using sunpath.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sunpath.Services.Interface
+{
+    public interface IVehicleService
+    {
+        Task<IEnumerable<Vehicle>>GetAllVehiclesAsync();//گرفتن لیست تمام خودرو ها برای لود اولیه در نقشه
+        Task UpdateVehicleStatusAsync(int id, double lat, double lng, double speed, double heading);
+
+    }
+}
