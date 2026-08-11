43841cc3f75b9900e3e3847478df64185af3bc3e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-11 12:14:52 +0330

    add connection string add write dbhelper

---
+++ sunpath/Data/DbHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sunpath.Data
+{
+    public class DbHelper
+    {
+            private readonly string _connectionString;
+            public DbHelper(IConfiguration configuration)
+            {
+                _connectionString = configuration.GetConnectionString("SunPathConnection");
+            }
+            public SqlConnection GetConnection()
+            {
+                return new SqlConnection(_connectionString);
+            }
+      
+    }
+}
--- sunpath/Startup.cs
+++ sunpath/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using sunpath.Data;
 
 namespace sunpath
 {
@@ -26,6 +27,7 @@ namespace sunpath
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddTransient<DbHelper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
