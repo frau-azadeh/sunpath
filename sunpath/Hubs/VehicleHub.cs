7fca4681549736312d7c1e17c00f11f505cda571
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-11 13:15:45 +0330

    add Hub

---
+++ sunpath/Hubs/VehicleHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sunpath.Hubs
+{
+    public class VehicleHub: Hub
+    {
+        public async Task NotifyConnected()
+        {
+            await Clients.Caller.SendAsync("Welcome", "خوش آمدید، شما به سیستم مانیتورینگ SunPath وصل شدید.");
+        }
+    }
+}
