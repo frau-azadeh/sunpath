5015eae2fb8ba3cb9b9482aa34659a43c5fc7695
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-13 19:40:25 +0330

    add hub

---
+++ sunpath/Hubs/DriverHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sunpath.Hubs
+{
+    public class DriverHub : Hub
+    {
+    }
+}
