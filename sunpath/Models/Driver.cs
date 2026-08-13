fcaec46ef11be697582f7fc527b093c89df77a6b
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-13 19:40:50 +0330

    add modal

---
+++ sunpath/Models/Driver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sunpath.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string NationalId { get; set; }
+
+        public string Phone { get; set; }
+
+        public int LicenseType { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
