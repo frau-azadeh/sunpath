185d00ae555167f4f575becf359706639353de1e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-13 19:41:54 +0330

    update ip

---
+++ sunpath/Services/Interface/IDriverRepository.cs
@@ -0,0 +1,18 @@
+﻿using sunpath.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sunpath.Services.Interface
+{
+    public interface IDriverRepository
+    {
+        List<Driver> GetAll();
+        Driver GetById(int id);
+        int Create(Driver driver);
+        bool Update(int id, Driver driver);
+        bool Delete(int id);
+        bool ExistsByNationalId(string nationalId, int? excludeId = null);
+    }
+}
--- sunpath/Startup.cs
+++ sunpath/Startup.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using sunpath.Data;
-using sunpath.Hubs;      // اضافه شده برای دسترسی به کلاس VehicleHub
-using sunpath.Services;  // اضافه شده برای دسترسی به IVehicleService و VehicleService
+using sunpath.Hubs;
+using sunpath.Services;
 using sunpath.Services.Implementation;
 using sunpath.Services.Interface;
 
@@ -27,30 +22,56 @@ namespace sunpath
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // ۱. تنظیم CORS برای برقراری ارتباط بدون خطای امنیت مرورگر با فرانت‌انند Next.js
             services.AddCors(options =>
             {
-                options.AddPolicy("CorsPolicy", builder => builder
-                    .WithOrigins("http://localhost:3000") // آدرس فرانت‌انند
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials()); // حیاتی برای کارکرد SignalR
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    var corsFilePath = Path.Combine(
+                        Directory.GetCurrentDirectory(),
+                        "wwwroot",
+                        "settings",
+                        "CorsOrigins.txt");
+
+                    string[] allowedOrigins = { "http://localhost:3000" };
+
+                    if (File.Exists(corsFilePath))
+                    {
+                        var fileContent = File.ReadAllLines(corsFilePath)
+                            .Where(line => !string.IsNullOrWhiteSpace(line))
+                            .Select(line => line.Trim())
+                            .ToArray();
+
+                        if (fileContent.Length > 0)
+                        {
+                            allowedOrigins = fileContent;
+                        }
+                    }
+
+                    builder.WithOrigins(allowedOrigins)
+                           .AllowAnyMethod()
+                           .AllowAnyHeader()
+                           .AllowCredentials();
+                });
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            // ۲. ثبت سرویس‌های برنامه در DI Container
             services.AddTransient<DbHelper>();
+
             services.AddScoped<IVehicleService, VehicleService>();
 
-            // ۳. اضافه کردن سرویس SignalR به پروژه
+            services.AddScoped<IDriverRepository, DriverRepository>();
+
+            services.AddSingleton<VehicleSimulationService>();
+            services.AddSingleton<Microsoft.Extensions.Hosting.IHostedService>(
+                provider => provider.GetRequiredService<VehicleSimulationService>());
+
             services.AddSignalR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -62,12 +83,10 @@ namespace sunpath
                 app.UseHsts();
             }
 
-            // ۴. فعال‌سازی سیاست CORS قبل از احراز هویت و روتینگ‌ها
             app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
-            // ۵. تعریف مسیرهای هاب SignalR
             app.UseSignalR(routes =>
             {
                 routes.MapHub<VehicleHub>("/vehicleHub");
