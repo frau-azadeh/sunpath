f4a3382b86a592b02dd67fe8226b9c390f73ac70
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-13 19:41:17 +0330

    add

---
+++ sunpath/Services/Implementation/VehicleRealtimeService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using sunpath.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sunpath.Services.Implementation
+{
+    public class VehicleRealtimeService
+    {
+
+        private readonly IHubContext<VehicleHub> _hubContext;
+
+        public VehicleRealtimeService(IHubContext<VehicleHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task BroadcastVehiclePositionAsync(
+            int vehicleId,
+            double latitude,
+            double longitude,
+            double speed,
+            double heading)
+        {
+            var payload = new
+            {
+                id = vehicleId,
+                latitude = latitude,
+                longitude = longitude,
+                speed = speed,
+                heading = heading,
+                timestamp = DateTime.UtcNow
+            };
+
+            await _hubContext.Clients.All.SendAsync("VehiclePositionChanged", payload);
+        }
+    }
+}
