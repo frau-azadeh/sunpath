d02bd00aff8759a9130bbb2defccc86f90838a26
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-13 19:40:36 +0330

    update

--- sunpath/Hubs/VehicleHub.cs
+++ sunpath/Hubs/VehicleHub.cs
@@ -1,16 +1,24 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace sunpath.Hubs
 {
-    public class VehicleHub: Hub
+    public class VehicleHub : Hub
     {
         public async Task NotifyConnected()
         {
-            await Clients.Caller.SendAsync("Welcome", "خوش آمدید، شما به سیستم مانیتورینگ SunPath وصل شدید.");
+            await Clients.Caller.SendAsync(
+                "Welcome",
+                "خوش آمدید، شما به سیستم مانیتورینگ SunPath وصل شدید."
+            );
+        }
+
+
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
