8430110a7125f2f14447c7f24ae37b8350b43b6d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-15 20:59:28 +0330

    add

---
+++ sunpath/Models/Dto/UpdateDriverDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sunpath.Models.Dto
+{
+    public class UpdateDriverDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string NationalId { get; set; }
+
+        public string Phone { get; set; }
+
+        public int LicenseType { get; set; }
+    }
+}
