ff12630a0a9e7a0b78edc2bf1ba7c7f85ba3a364
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-16 21:47:38 +0330

    update

--- sunpath/Models/Vehicle.cs
+++ sunpath/Models/Vehicle.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace sunpath.Models
 {
@@ -9,11 +6,28 @@ namespace sunpath.Models
     {
         public int Id { get; set; }
         public string PlateNumber { get; set; }
-        public string Status { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public string Model { get; set; }
+        public int Status { get; set; }
+
+        public decimal? LastLatitude { get; set; }
+        public decimal? LastLongitude { get; set; }
+        public DateTime? LastUpdateAt { get; set; }
+
         public double Speed { get; set; }
         public double Heading { get; set; }
-        public DateTime LastUpdate { get; set; }
+
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public DateTime? LastUpdate { get; set; }
+
+        public int VehicleType { get; set; }
+        public string InsuranceNumber { get; set; }
+        public DateTime? InsuranceExpiryDate { get; set; }
+        public int? CurrentDriverId { get; set; }
+
+        // برای نمایش در UI
+        public string CurrentDriverName { get; set; }
+        public string VehicleTypeName { get; set; }
+        public string StatusName { get; set; }
     }
 }
