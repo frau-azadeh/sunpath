f6ff9ecb7d2591384ae1a650c076052288ecea15
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-20 22:29:33 +0330

    add

---
+++ sunpath/Models/Dto/UpdateDispatchStatusRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sunpath.Models.Dto
+{
+    public class UpdateDispatchStatusRequest
+    {
+        public string Status { get; set; }
+    }
+}
