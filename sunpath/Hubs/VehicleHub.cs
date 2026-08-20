48075d0489e46f09156fe7c3a7fc1a68ceb53ecb
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-20 22:29:22 +0330

    update

--- sunpath/Hubs/VehicleHub.cs
+++ sunpath/Hubs/VehicleHub.cs
@@ -20,5 +20,37 @@ namespace sunpath.Hubs
         {
             await base.OnDisconnectedAsync(exception);
         }
+        public Task SubscribeVehicle(int vehicleId)
+        {
+            return Groups.AddToGroupAsync(
+                Context.ConnectionId,
+                "vehicle-" + vehicleId);
+        }
+
+        public Task UnsubscribeVehicle(int vehicleId)
+        {
+            return Groups.RemoveFromGroupAsync(
+                Context.ConnectionId,
+                "vehicle-" + vehicleId);
+        }
+
+        /*
+         * برای صفحه‌ی نقشه‌ی کلی؛
+         * در حال حاضر فقط اتصال را نگه می‌دارد.
+         */
+        public Task SubscribeLiveMap()
+        {
+            return Groups.AddToGroupAsync(
+                Context.ConnectionId,
+                "live-map");
+        }
+
+        public Task UnsubscribeLiveMap()
+        {
+            return Groups.RemoveFromGroupAsync(
+                Context.ConnectionId,
+                "live-map");
+        }
     }
 }
+
