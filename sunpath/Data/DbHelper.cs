14d88b7b4a2e6f388dfc98696dde530b4709b370
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-20 22:29:12 +0330

    add

--- sunpath/Data/DbHelper.cs
+++ sunpath/Data/DbHelper.cs
@@ -1,23 +1,105 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace sunpath.Data
 {
     public class DbHelper
     {
-            private readonly string _connectionString;
-            public DbHelper(IConfiguration configuration)
+        private readonly string _connectionString;
+
+        public DbHelper(IConfiguration configuration)
+        {
+            _connectionString =
+                configuration.GetConnectionString("SunPathConnection");
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        public async Task<object> ExecuteScalarAsync(
+            string query,
+            params SqlParameter[] parameters)
+        {
+            using (var connection = GetConnection())
+            using (var command = new SqlCommand(query, connection))
             {
-                _connectionString = configuration.GetConnectionString("SunPathConnection");
+                command.CommandType = CommandType.Text;
+
+                if (parameters != null && parameters.Length > 0)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                await connection.OpenAsync();
+
+                return await command.ExecuteScalarAsync();
             }
-            public SqlConnection GetConnection()
+        }
+
+        public async Task<int> ExecuteNonQueryAsync(
+            string query,
+            params SqlParameter[] parameters)
+        {
+            using (var connection = GetConnection())
+            using (var command = new SqlCommand(query, connection))
             {
-                return new SqlConnection(_connectionString);
+                command.CommandType = CommandType.Text;
+
+                if (parameters != null && parameters.Length > 0)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                await connection.OpenAsync();
+
+                return await command.ExecuteNonQueryAsync();
             }
-      
+        }
+
+        public async Task<List<Dictionary<string, object>>> ExecuteQueryAsync(
+            string query,
+            params SqlParameter[] parameters)
+        {
+            var result = new List<Dictionary<string, object>>();
+
+            using (var connection = GetConnection())
+            using (var command = new SqlCommand(query, connection))
+            {
+                command.CommandType = CommandType.Text;
+
+                if (parameters != null && parameters.Length > 0)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                await connection.OpenAsync();
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        var row = new Dictionary<string, object>();
+
+                        for (var i = 0; i < reader.FieldCount; i++)
+                        {
+                            row[reader.GetName(i)] =
+                                reader.IsDBNull(i)
+                                    ? null
+                                    : reader.GetValue(i);
+                        }
+
+                        result.Add(row);
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
