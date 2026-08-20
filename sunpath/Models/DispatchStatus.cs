b238ac04023b802c07988af0d22cee9034576f1c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-20 22:29:49 +0330

    add

---
+++ sunpath/Models/DispatchStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sunpath.Models
+{
+    public enum DispatchStatus
+    {
+        Pending = 0,
+
+        // مأموریت به راننده و خودرو تخصیص داده شده
+        Assigned = 1,
+
+        // راننده مأموریت را شروع کرده است
+        Started = 2,
+
+        Completed = 3,
+
+        Cancelled = 4
+    }
+}
