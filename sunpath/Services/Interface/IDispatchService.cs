35436cb7d3bcc9828ec930c4ad568c7c1a26a370
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-20 22:30:11 +0330

    add

---
+++ sunpath/Services/Interface/IDispatchService.cs
@@ -0,0 +1,25 @@
+﻿using sunpath.Models;
+using sunpath.Models.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace sunpath.Services.Interface
+{
+    public interface IDispatchService
+    {
+        Task<int> CreateAsync(CreateDispatchRequest request);
+
+        Task<Dispatch> GetByIdAsync(int id);
+
+        Task<List<Dispatch>> GetAllAsync();
+
+        Task<bool> UpdateStatusAsync(
+            int id,
+            UpdateDispatchStatusRequest request);
+
+        Task<bool> UpdateVehicleLocationAsync(
+            UpdateVehicleLocationRequest request);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
