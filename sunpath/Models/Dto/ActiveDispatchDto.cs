56d910484dd9720b0a3b74e572449846646d7973
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-09-10 20:51:28 +0330

    update

---
+++ sunpath/Models/Dto/ActiveDispatchDto.cs
@@ -0,0 +1,10 @@
+using sunpath.Models;
+
+namespace sunpath.Models.Dto
+{
+    public class ActiveDispatchDto
+    {
+        public Dispatch Dispatch { get; set; }
+        public Vehicle Vehicle { get; set; }
+    }
+}
